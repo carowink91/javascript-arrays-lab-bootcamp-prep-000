02ac25279ad0e0fd15d7969701371e248491d3c5
angelica winkler <caw400@nyu.edu>
2018-04-16 17:45:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function kittens () {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten (name) {
+  var newKittens = kittens.
+}
